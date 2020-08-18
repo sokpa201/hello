2f737454b2b86c987427ce0b2530285a76415ab8
Darian Sokpa <dsokpa2@huskers.unl.edu>
2020-08-18 22:00:37 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Darian Sokpa
+ * Date: 2020
+ *
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv) {
+
+     printf("Darian Sokpa!\n");
+     printf("Computer Science\n");
+
+     return 0;
+
+ }
